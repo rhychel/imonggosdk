dd353aea6fb92dc40a9b5074559c9504d94ad7a4
Jan <jan@nueca.net>
2015-07-31 20:23:30 +0800

    Added AutoUpdate Support.

--- concessio/src/main/java/net/nueca/concessio/C_Login.java
+++ concessio/src/main/java/net/nueca/concessio/C_Login.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.util.Log;
 
 import net.nueca.concessioengine.activities.login.LoginActivity;
+import net.nueca.imonggosdk.enums.Server;
+import net.nueca.imonggosdk.enums.SettingsName;
+import net.nueca.imonggosdk.enums.Table;
 import net.nueca.imonggosdk.tools.LoggingTools;
+import net.nueca.imonggosdk.tools.SettingTools;
 
 import java.sql.SQLException;
 
@@ -20,14 +24,28 @@ public class C_Login extends LoginActivity {
     }
 
     @Override
+    protected void initLoginEquipments() {
+        super.initLoginEquipments();
+        setServer(Server.IRETAILCLOUD_COM);
+
+        // set the Modules to download
+        int[] modules = {Table.USERS.ordinal(), Table.PRODUCTS.ordinal(), Table.UNITS.ordinal()};
+        setModules(modules);
+
+        SettingTools.updateSettings(this, SettingsName.AUTO_UPDATE, true, "");
+    }
+
+
+    @Override
     protected void showNextActivity() {
+
         finish();
         Intent intent = new Intent(this, C_Module.class);
         startActivity(intent);
 
         try {
             if (getSession().getUser() != null)
-                LoggingTools.showToast(C_Login.this, getSession().getUser().getName());
+                LoggingTools.showToast(C_Login.this, "Welcome " + getSession().getUser().getName() + "!");
             Log.e(TAG, getHelper().getUnits().queryForAll().get(0).getProduct().getName());
         } catch (SQLException e) {
             e.printStackTrace();
