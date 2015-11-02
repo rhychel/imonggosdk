e123dfb0d4652fa2022c59cab8957a8b9766fb56
raquezha <j$GV?8.-4ioY>
2015-11-02 20:16:33 +0800

    Removed unnecessary files. 2

--- concessioengine/src/main/java/net/nueca/concessioengine/animations/SlideInUpAnimation.java
+++ concessioengine/src/main/java/net/nueca/concessioengine/animations/SlideInUpAnimation.java
@@ -1,49 +1,50 @@
 package net.nueca.concessioengine.animations;
 
-import android.support.v7.widget.RecyclerView;
+import android.app.Activity;
+
 
 /**
  * Created by rhymart on 7/15/15.
  * imonggosdk (c)2015
  */
-public class SlideInUpAnimation extends RecyclerView.ItemAnimator {
-    @Override
-    public void runPendingAnimations() {
-
-    }
-
-    @Override
-    public boolean animateRemove(RecyclerView.ViewHolder holder) {
-        return false;
-    }
-
-    @Override
-    public boolean animateAdd(RecyclerView.ViewHolder holder) {
-        return false;
-    }
-
-    @Override
-    public boolean animateMove(RecyclerView.ViewHolder holder, int fromX, int fromY, int toX, int toY) {
-        return false;
-    }
-
-    @Override
-    public boolean animateChange(RecyclerView.ViewHolder oldHolder, RecyclerView.ViewHolder newHolder, int fromLeft, int fromTop, int toLeft, int toTop) {
-        return false;
-    }
-
-    @Override
-    public void endAnimation(RecyclerView.ViewHolder item) {
-
-    }
-
-    @Override
-    public void endAnimations() {
-
-    }
-
-    @Override
-    public boolean isRunning() {
-        return false;
-    }
+public class SlideInUpAnimation extends Activity {
+//    @Override
+//    public void runPendingAnimations() {
+//
+//    }
+//
+//    @Override
+//    public boolean animateRemove(RecyclerView.ViewHolder holder) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean animateAdd(RecyclerView.ViewHolder holder) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean animateMove(RecyclerView.ViewHolder holder, int fromX, int fromY, int toX, int toY) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean animateChange(RecyclerView.ViewHolder oldHolder, RecyclerView.ViewHolder newHolder, int fromLeft, int fromTop, int toLeft, int toTop) {
+//        return false;
+//    }
+//
+//    @Override
+//    public void endAnimation(RecyclerView.ViewHolder item) {
+//
+//    }
+//
+//    @Override
+//    public void endAnimations() {
+//
+//    }
+//
+//    @Override
+//    public boolean isRunning() {
+//        return false;
+//    }
 }
