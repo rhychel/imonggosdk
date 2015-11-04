79d3b139a3bdb4f062ea5c1dfa037e1b1fceeaf4
raquezha <j$GV?8.-4ioY>
2015-11-04 17:59:57 +0800

    delete salesdashboard

--- salesdashboard/src/main/java/nueca/net/salesdashboard/interfaces/OnReloadBranches.java
+++
@@ -1,8 +0,0 @@
-package nueca.net.salesdashboard.interfaces;
-
-
-public interface OnReloadBranches {
-    void finishedReloading();
-    void showBasicDialogMessageReloadBranches(String message, String title);
-    void showProgressHudReloadBranches(String message);
-}
