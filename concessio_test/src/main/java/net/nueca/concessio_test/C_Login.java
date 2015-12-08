f0cf608fe20bb703e0d467f165f8ef5cfff0d5e3
raquezha <j$GV?8.-4ioY>
2015-12-08 16:19:14 +0800

    dev2 > jn

--- concessio_test/src/main/java/net/nueca/concessio_test/C_Login.java
+++ concessio_test/src/main/java/net/nueca/concessio_test/C_Login.java
@@ -47,18 +47,14 @@ public class C_Login extends LoginActivity {
                 Table.BRANCH_USERS.ordinal(),
                 Table.CUSTOMER_CATEGORIES.ordinal(),
                 Table.CUSTOMER_GROUPS.ordinal(),
-                Table.BRANCH_CUSTOMERS.ordinal(),
-//                Table.BRANCH_PRODUCTS.ordinal(),
-                Table.BRANCH_UNITS.ordinal(),
-                Table.BRANCH_PRICE_LISTS.ordinal(),
-                //Table.PRICE_LISTS_DETAILS.ordinal(),
-                Table.PAYMENT_TYPES.ordinal(),
-                Table.PAYMENT_TERMS.ordinal(),
-                Table.INVOICES.ordinal(),
-                Table.INVOICE_PURPOSES.ordinal(),
-                Table.SALES_PUSH.ordinal()
-                //Table.SALES_PROMOTIONS_DISCOUNT.ordinal(),
-                /*Table.BRANCH_ROUTE_PLANS.ordinal()*/);
+                Table.CUSTOMERS.ordinal(),
+                Table.BRANCH_PRODUCTS.ordinal(),
+                Table.UNITS.ordinal(),
+                Table.PRICE_LISTS.ordinal());
+
+             /*   Table.UNITS.ordinal(),
+                Table.CUSTOMER_GROUPS.ordinal(),
+                Table.PRICE_LISTS.ordinal());*/
 
     }
 
