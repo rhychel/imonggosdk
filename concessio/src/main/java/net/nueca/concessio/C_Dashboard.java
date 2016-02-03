e0f877ae9bcca2e55b0ff35e0e706f67a692c4c9
raquezha <j$GV?8.-4ioY>
2016-02-03 15:07:23 +0800

    backup jn

--- concessio/src/main/java/net/nueca/concessio/C_Dashboard.java
+++ concessio/src/main/java/net/nueca/concessio/C_Dashboard.java
@@ -14,6 +14,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import net.nueca.concessioengine.activities.DashboardActivity;
+import net.nueca.concessioengine.activities.SettingsActivity;
 import net.nueca.concessioengine.activities.module.ModuleActivity;
 import net.nueca.concessioengine.adapters.DashboardRecyclerAdapter;
 import net.nueca.concessioengine.adapters.interfaces.OnItemClickListener;
