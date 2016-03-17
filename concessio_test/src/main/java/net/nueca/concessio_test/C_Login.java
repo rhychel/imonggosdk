3f0177f0aa09261548905726855185ac5d57ea56
Jn Cld <jan@nueca.net>
2016-03-17 15:04:04 +0800

    + Fix Sync Paging of Details (Route Details, Sales Points, Discounts & Price Lists)

--- concessio_test/src/main/java/net/nueca/concessio_test/C_Login.java
+++ concessio_test/src/main/java/net/nueca/concessio_test/C_Login.java
@@ -40,29 +40,29 @@ public class C_Login extends LoginActivity {
 
         setModulesToSync(
                 Table.USERS.ordinal(),
-                Table.BRANCH_USERS.ordinal(),
+                /*Table.BRANCH_USERS.ordinal(),
                 Table.SETTINGS.ordinal(),
                 Table.PRODUCTS.ordinal(),
                 Table.UNITS.ordinal(),
-                /*Table.CUSTOMER_CATEGORIES.ordinal(),
-                Table.CUSTOMER_BY_SALESMAN.ordinal(),
+                Table.CUSTOMER_CATEGORIES.ordinal(),
+                Table.CUSTOMER_BY_SALESMAN.ordinal(),*/
                 Table.ROUTE_PLANS.ordinal(),
-                Table.ROUTE_PLANS_DETAILS.ordinal(),
+                Table.ROUTE_PLANS_DETAILS.ordinal(),/*,
                 Table.BRANCH_PRODUCTS.ordinal(),*/
-                Table.ACCOUNT_PRICES.ordinal(),
+                /*Table.ACCOUNT_PRICES.ordinal(),
                 Table.DOCUMENTS.ordinal(),
                 Table.DOCUMENT_TYPES.ordinal(),
-                Table.DOCUMENT_PURPOSES.ordinal()
-               /* Table.PRICE_LISTS_FROM_CUSTOMERS.ordinal(),
-                Table.PRICE_LISTS_DETAILS.ordinal()
-                Table.PAYMENT_TERMS.ordinal(),
+                Table.DOCUMENT_PURPOSES.ordinal()*/
+                Table.PRICE_LISTS_FROM_CUSTOMERS.ordinal(),
+                Table.PRICE_LISTS_DETAILS.ordinal(),
+                /*Table.PAYMENT_TERMS.ordinal(),
                 Table.PAYMENT_TYPES.ordinal(),
                 Table.INVOICES.ordinal(), */
-               /* Table.INVOICE_PURPOSES.ordinal(),
+                Table.INVOICE_PURPOSES.ordinal(),
                 Table.SALES_PROMOTIONS_SALES_DISCOUNT.ordinal(),
                 Table.SALES_PROMOTIONS_SALES_DISCOUNT_DETAILS.ordinal(),
                 Table.SALES_PROMOTIONS_POINTS.ordinal(),
-                Table.SALES_PROMOTIONS_POINTS_DETAILS.ordinal()*/);
+                Table.SALES_PROMOTIONS_POINTS_DETAILS.ordinal());
     }
 
     @Override
@@ -76,7 +76,7 @@ public class C_Login extends LoginActivity {
     protected void onCreateLoginLayout() {
         super.onCreateLoginLayout();
         setEditTextAccountID("A1029");
-        setEditTextEmail("elvin29@imonggo.com");
+        setEditTextEmail("A1072A_OSS-1@imonggo.com");
         setEditTextPassword("password");
     }
 }
